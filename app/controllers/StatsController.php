72f71eddb6f9ba4b559c506d936223ce0e32bab4
Sanil Khamkar <sanilpsy@gmail.com>
2022-06-27 16:14:50 -0500

    Add files via upload

---
+++ app/controllers/StatsController.php
@@ -0,0 +1,40 @@
+<?php
+class StatsController {
+    public function index() {
+              
+       
+
+        return App::view('stats', [
+            
+        ]);
+    }
+}
+
+
+public function driverstats () {
+
+    $commons = [
+        'Zhou' => [
+        ]
+    ];
+
+    return $data;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
