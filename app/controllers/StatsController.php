0ecc1d20e9c51a3240644b086f4f16ddd05958e4
SanilKhamkar <sanilpsy@gmail.com>
2022-06-27 21:47:29 -0500

    Updated Driver Design and array

--- app/controllers/StatsController.php
+++ app/controllers/StatsController.php
@@ -2,39 +2,46 @@
 class StatsController {
     public function index() {
               
-       
+       $driverdata = $this->driverstats();
 
         return App::view('stats', [
-            
+            'drivers' => $driverdata,
         ]);
     }
+
+    public function driverstats () {
+
+        $commons = [
+            'Zhou' => [
+                'Rarity' => 'Common Series 1',
+                'Stats' => [
+                    'Overtaking' => '12',
+                    'Defending' => '7',
+                    'Consistency' => '11',
+                    'Fuel' => '9',
+                    'Tyre' => '5',
+                    'Wetness' => '3', 
+                ],
+            ],
+
+            'Kevin' => [
+                'Rarity' => 'Common Series 1',
+                'Stats' => [
+                    'Overtaking' => '9',
+                    'Defending' => '13',
+                    'Consistency' => '4',
+                    'Fuel' => '11',
+                    'Tyre' => '7',
+                    'Wetness' => '5', 
+                ],
+            ],
+        ];
+
+        return $commons;
+    }
+
+
+
+
 }
-
-
-public function driverstats () {
-
-    $commons = [
-        'Zhou' => [
-        ]
-    ];
-
-    return $data;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
--- app/views/stats.php
+++ app/views/stats.php
@@ -4,3 +4,45 @@ $this->layout('template::main', [
     'pageDesc' => "...",
 ]);
 ?>
+
+<?php 
+    echo '<pre style="max-height:600px; overflow-y: auto; border:1px solid #000;">';
+    
+    foreach($drivers as $drivername=>$data) {
+        echo $drivername . '=>' . $data;
+    }
+
+    echo '</pre>';
+?>
+
+<div class="container">
+    <div class="row">
+        <?php 
+            foreach($drivers as $drivername=>$data) {
+        ?>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title"><?=$drivername?></h5>
+                <table class="table table-dark table-hover">
+                    <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+        </div>
+        <?php 
+            }
+        ?>
+    </div>
+</div>
