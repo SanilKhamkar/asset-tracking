ccef94e945a48981f66c48ac5a868dbad10e6599
Sanil Khamkar <sanilpsy@gmail.com>
2022-07-06 16:20:22 -0500

    Added boosts view

---
+++ app/views/boosts.php
@@ -0,0 +1,24 @@
+<?php 
+$this->layout('template::main', [
+    'pageTitle' => 'Statistics',
+    'pageDesc' => "...",
+]);
+?>
+
+<div class="container">
+    <div class="row mt-3">
+        <div class="col border-bottom border-info border-3">
+            <p class="fs-1">BOOSTS</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title border-bottom border-info border-3"><p class=""></p></h5>
+            </div>
+        </div>
+    </div>
+</div>
