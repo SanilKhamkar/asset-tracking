b7e44713b54a8d6f05727f32cf00febc565d9842
Sanil Khamkar <sanilpsy@gmail.com>
2022-07-07 16:18:05 -0500

    Updated Standard Boosts

--- app/controllers/BoostController.php
+++ app/controllers/BoostController.php
@@ -2,10 +2,10 @@
 class BoostController {
     public function index() {
               
-       
+        $standardboostdata = $this->standardboosts();
 
         return App::view('boosts', [
-            
+            'standardboostdata' => $standardboostdata,
         ]);
     }
 
@@ -14,12 +14,60 @@ class BoostController {
 
         $boost = [
             'Self Control' => [
-                'Fuel' => 'I',
-                'Tyre' => 'I',
-                ''
+                'stats' => [
+                    'Fuel' => 'I',
+                    'Tyre' => 'I',
+                ],
+                'Img' => '/resources/images/standard_boost/self_control.png', 
             ],
 
+            'Warrior' => [
+                'stats' => [
+                    'Block' => 'I',
+                    'Fuel' => 'II',
+                    'Overtake' => 'III',
+                ],
+                'Img' => '/resources/images/standard_boost/warrior.png', 
+            ],
+
+            'Ballast' => [
+                'stats' => [
+                    'Corner' => 'I',
+                    'Grip' => 'I',
+                    'Wetness' => 'II',
+                ],
+                'Img' => '/resources/images/standard_boost/ballast.png', 
+            ],
+
+            'Instinct' => [
+                'stats' => [
+                    'Fuel' => 'III',
+                    'Overtake' => 'II',
+                    'Speed' => 'III',
+                ],
+                'Img' => '/resources/images/standard_boost/instinct.png', 
+            ],
+
+            'Downforce' => [
+                'stats' => [
+                    'Corner' => 'II',
+                    'Grip' => 'II',
+                    'Tyre' => 'III',
+                ],
+                'Img' => '/resources/images/standard_boost/downforce.png', 
+            ],
+
+            'Hex' => [
+                'stats' => [
+                    'Overtake' => 'V',
+                    'Speed' => 'IV',
+                    'Wetness' => 'I',
+                ],
+                'Img' => '/resources/images/standard_boost/hex.png', 
+            ],
         ];
+
+    return $boost;
     }
 }
 ?>
